012110c140b4c42767d96adb63a834c03ec6f263
ceisenhauer <eisenhauer.catherine@gmail.com>
2022-07-12 11:40:17 +0100

    get date of iso week

---
+++ R/get_week_start.R
@@ -0,0 +1,15 @@
+#' Start of Week
+#'
+#' @description Gets the first monday of a given ISO week-year.
+#'
+#' @param week `int/str` ISO week number, zero padding is accepted but not required.
+#' @param year `int/str` ISO year. Must be formatted as YYYY. 
+#'
+#' @importFrom stringr str_pad
+#' @importFrom ISOweek ISOweek2date
+get_week_start <- function(week, year) {
+  week <- stringr::str_pad(week, 2, pad = '0')
+  date <- ISOweek::ISOweek2date(paste0(year, '-W', week, '-1'))
+
+  return(date)
+}
