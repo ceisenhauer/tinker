b8ce81783fb8fd03a841a141c8318f33aa98a117
ceisenhauer <eisenhauer.catherine@gmail.com>
2022-07-14 14:45:56 +0100

    idkwid, but probs this function will be superseeded by iso_to_week

--- R/get_week_start.R
+++ R/get_week_start.R
@@ -8,12 +8,9 @@
 #' @importFrom stringr str_pad
 #' @importFrom ISOweek ISOweek2date
 get_week_start <- function(week, year) {
-  if (is.na(week) | is.na(year)) {
-    date <- NA
-  } else {
-    week <- stringr::str_pad(week, 2, pad = '0')
-    date <- ISOweek::ISOweek2date(paste0(year, '-W', week, '-1'))
-  }
+  week <- stringr::str_pad(week, 2, pad = '0')
+  date <- ISOweek::ISOweek2date(paste0(year, '-W', week, '-1'))
 
   return(date)
 }
+
